b2f53db9d2a31a0b340ae8e5cbbc15b64f7402cd
Piyush <piyuinshort@gmail.com>
2021-02-23 11:40:48 +0530

    works!

---
+++ script.js
@@ -0,0 +1,20 @@
+const boxesContainer = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.backgroundPosition = `${-j * 125}px ${-125 * i}px`;
+      boxesContainer.appendChild(box);
+    }
+  }
+}
+
+createBoxes();
+
+btn.addEventListener('click', () => {
+  boxesContainer.classList.toggle('big');
+  //   console.log('clicked');
+});
